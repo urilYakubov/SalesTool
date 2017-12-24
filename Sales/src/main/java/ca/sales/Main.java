9d0dab3aa88305d58f9df332b30c251b46502ab9
uril <uril@DESKTOP-V2V3E2I>
2017-12-24 15:14:06 -0500

    Apply stashed.

--- Sales/src/main/java/ca/sales/Main.java
+++ Sales/src/main/java/ca/sales/Main.java
@@ -5,9 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("HELLO SALES");
 		System.out.println("HELLO SALES!!!");
-		System.out.println("Test1");
-		
+		System.out.println("Test1");		
+
 		System.out.println("Test2");
+		System.out.println("Stached");
+
 	}
 
 }
